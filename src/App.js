d00dd697b29ef86889a4a25cdfcc87d624551679
samlak <samlak1999@gmail.com>
2020-04-15 10:27:22 +0100

    project initilization for http request

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from 'react';
+
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
-  state = {
-    persons: [
-      {id: 'cbdhdhd', name: "Samlak", age: 20},
-      {id: 'wwdhdhd', name: "Adebayo", age: 26},
-      {id: 'cbdhwad', name: "Badejoko", age: 23},
-    ],
-    otherState: 'some other value',
-    showPersons: false,
-    textLength: 0,
-    chars: [],
-    charString: '',
-  }
-
   render() {
     return (
       <div className="App">
-        <h1>Start writing your React code here</h1>
-      </div> 
+        <Blog />
+      </div>
     );
-  } 
+  }
 }
 
 export default App;
